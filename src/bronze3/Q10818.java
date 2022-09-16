734864be1b134b745222fd0e70516e7acf8494bb
monya-9 <lee99714@naver.com>
2022-09-16 17:45:08 +0900

    최소, 최대

--- src/bronze3/Q10818.java
+++ src/bronze3/Q10818.java
@@ -8,16 +8,15 @@ public class Q10818 {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int[] num = new int[size];
-		
-		int max = 0;
-		int min = 0;
-		
+
 		for (int i = 0; i < size; i++) {
 			num[i] = sc.nextInt();
 			
-			if(num[i] > max) {
-				max = num[i];
-			}
+		}
+		Arrays.sort(num);
+		
+		for (int i = 0; i < 1; i++) {
+			System.out.println(num[0] + " " + num[size-1]);			
 		}
 	}
 }
