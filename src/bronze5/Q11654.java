168947f3e8f37def78087333588366b854cd16f5
monya-9 <lee99714@naver.com>
2022-10-01 17:58:05 +0900

    아스키코드

---
+++ src/bronze5/Q11654.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Q11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		int num = (int)ch;
+		
+		System.out.println(num);
+	}
+}
