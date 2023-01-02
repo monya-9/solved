a4d32f9c806ff1fbbab2e16fd0a2e0fb8b1f0a7d
monya-9 <lee99714@naver.com>
2023-01-02 21:22:40 +0900

    백준/솔브드 14720 우유 축제, 그리디 알고리즘

---
+++ src/bronze3/Q14720.java
@@ -0,0 +1,29 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class Q14720 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] milk = new int[N];
+		int cnt = 0;
+		int next = 0;
+
+		for (int i = 0; i < N; i++) {
+			milk[i] = sc.nextInt();
+
+			if (milk[i] == 0 && next == 0) {
+				cnt++;
+				next = 1;
+			} else if(milk[i] == 1 && next == 1) {
+				cnt++;
+				next = 2;
+			}else if(milk[i] == 2 && next == 2) {
+				cnt++;
+				next = 0;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
