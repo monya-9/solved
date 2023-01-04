bcb83e3400737193cd94a3dd98601dc41dd136f2
monya-9 <lee99714@naver.com>
2023-01-04 13:48:44 +0900

    백준 15552 빠른 A+B - 버퍼쓰기

---
+++ src/bronze4/Q15552.java
@@ -0,0 +1,25 @@
+package bronze4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = null;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write(a+b + "\n");	//버퍼 int출력시 + " "를 해줘야 문자열출력?
+		}
+		bw.flush();
+	}
+}
