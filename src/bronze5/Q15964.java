7f9fe71494bc1b8c7444bbfe56f908b5051cb58f
monya-9 <lee99714@naver.com>
2023-01-11 23:42:37 +0900

    백준, 솔브드 15964 이상한 기호 - 수학, 구현, 사칙연산

---
+++ src/bronze5/Q15964.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Q15964 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextInt();
+		long B = sc.nextInt();
+		System.out.println((A+B)*(A-B));
+	}
+}
