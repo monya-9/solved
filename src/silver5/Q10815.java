d278e2613d3f03a78b8b915f67ac1a0afd8db58b
monya-9 <lee99714@naver.com>
2023-02-06 10:25:25 +0900

    백준, 솔브드 10815 숫자 카드 - 자료구조, 이분탐색

--- src/silver5/Q10815.java
+++ src/silver5/Q10815.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Q10815 {	//이분 탐색
+public class Q10815 {	//이진 탐색
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
