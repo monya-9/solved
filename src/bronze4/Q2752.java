3c8eca0dfb6c291d69c393c9433b363edf9f0586
monya-9 <lee99714@naver.com>
2023-02-11 15:25:21 +0900

    백준, 솔브드 2752 세수정렬 - 정렬

---
+++ src/bronze4/Q2752.java
@@ -0,0 +1,20 @@
+package bronze4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q2752 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
