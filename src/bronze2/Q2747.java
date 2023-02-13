7e59da7b514542b17e150a5fdd74cadbea5569da
monya-9 <lee99714@naver.com>
2023-02-13 10:48:18 +0900

    백준, 솔브드 2747 파보나치 수 - 수학

---
+++ src/bronze2/Q2747.java
@@ -0,0 +1,20 @@
+package bronze2;
+
+import java.util.Scanner;
+
+public class Q2747 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n + 1];
+		
+		arr[0] = 0;
+		if(n > 0) {
+			arr[1] = 1;
+		}
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 2] + arr[i - 1];
+		}
+		System.out.println(arr[n]);
+	}
+}
