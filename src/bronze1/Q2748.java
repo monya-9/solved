dd66a4d21134801f720793989dc0e90d29e5eb9a
monya-9 <lee99714@naver.com>
2023-02-13 10:56:22 +0900

    백준, 솔브드 2748 파보나치 수 2 - 수학

---
+++ src/bronze1/Q2748.java
@@ -0,0 +1,24 @@
+package bronze1;
+
+import java.util.Scanner;
+
+public class Q2748 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] arr = new long[n + 1];
+		
+		arr[0] = 0;
+		if(n > 0) {
+			arr[1] = 1;
+		}
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 2] + arr[i - 1];
+		}
+		if(arr[n] < 0) {
+			System.out.println(-arr[n]);			
+		} else {
+			System.out.println(arr[n]);
+		}
+	}
+}
