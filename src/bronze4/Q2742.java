8c72a8959f720c2f812e358d8905e057895bb8fd
monya-9 <lee99714@naver.com>
2023-02-20 11:31:22 +0900

    백준, 솔브드 2742 기찍 N

---
+++ src/bronze4/Q2742.java
@@ -0,0 +1,15 @@
+package bronze4;
+
+import java.util.Scanner;
+
+public class Q2742 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		while(N > 0) {
+			System.out.println(N);
+			N--;
+		}
+	}
+}
