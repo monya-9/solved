929d5efbb2694744ec5f86e3e6dca2cc09643e9b
monya-9 <64175828+monya-9@users.noreply.github.com>
2024-04-01 22:40:49 +0900

    [백준/solved] 15680 연세대학교

---
+++ src/bronze5/Q15680.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Q15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();		
+		String str = (n == 0) ? "YONSEI" : "Leading the Way to the Future";
+		
+		System.out.println(str);
+	}
+}
