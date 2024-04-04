981dc18ebf65f7fee067720d56e90d4e321931dd
monya-9 <64175828+monya-9@users.noreply.github.com>
2024-04-04 20:53:12 +0900

    [백준 / solved] 11005 - 진법 변환2

---
+++ src/bronze1/Q11005.java
@@ -0,0 +1,13 @@
+package bronze1;
+
+import java.util.Scanner;
+
+public class Q11005 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(Integer.toString(a, b).toUpperCase());
+	}
+}
