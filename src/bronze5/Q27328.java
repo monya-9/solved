c8282400df8c496795b853e6acc6f91f9d82b399
monya-9 <64175828+monya-9@users.noreply.github.com>
2024-04-06 23:32:02 +0900

    [백준/Solved] - 27328 三方比較 (Three-Way Comparison)

---
+++ src/bronze5/Q27328.java
@@ -0,0 +1,20 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Q27328 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a < b) {
+			System.out.println("-1");
+		}else if(a == b) {
+			System.out.println("0");
+		}else {
+			System.out.println("1");
+		}
+	}
+}
