9ac01e7ad77fe8a2ac5777b76ecc55510b93cb18
monya-9 <64175828+monya-9@users.noreply.github.com>
2024-09-22 17:30:11 +0900

    [백준/Solved] 2775 - 부녀회장이 될테야

--- src/bronze1/Q2775.java
+++ src/bronze1/Q2775.java
@@ -7,20 +7,24 @@ public class Q2775 {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 
-		for (int i = 0; i < T; i++) {
+		int[][] APT = new int[15][15];
+		 
+		for(int i = 0; i < 15; i++) {
+			APT[i][1] = 1;
+			APT[0][i] = i;
+		}
+ 
+		for(int i = 1; i < 15; i ++) {
+ 
+			for(int j = 2; j < 15; j++) {
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+		
+		for(int i = 0; i < T; i++) {
 			int k = sc.nextInt();
 			int n = sc.nextInt();
-
-			System.out.println(fib(k, n));
+			System.out.println(APT[k][n]);
 		}
-	}
-
-	private static int fib(int k, int n) {
-		if (k == 0)
-			return n;
-		if (n == 0)
-			return 0;
-		else
-			return fib(k, n - 1) + fib(k - 1, n);
 	}
 }
