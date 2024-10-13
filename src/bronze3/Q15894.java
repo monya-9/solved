154ac626f61855588c8d11134befdd51e4865c49
monya-9 <64175828+monya-9@users.noreply.github.com>
2024-10-13 12:08:13 +0900

    [백준/solved] 15894 - 수학은 체육과목 입니다

---
+++ src/bronze3/Q15894.java
@@ -0,0 +1,12 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class Q15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		
+		System.out.println(n*4);
+	}
+}
